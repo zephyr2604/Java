cb8b897cd9f3aa662a1e2a3bbb1ebb0592a913db
zephyr2604 <aqib.sy@gmail.com>
2019-07-23 14:52:39 +0530

    1st commit

---
+++ src/main/java/com/apps/App.java
@@ -0,0 +1,13 @@
+package com.apps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
